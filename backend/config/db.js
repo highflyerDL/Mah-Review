aa33a2ff1b6e830393173809182d60be570661f4
Huy Phan <huy.phan@metropolia.fi>
2016-10-05 20:54:16 +0300

    Update readme and added dump data to restore

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,18 @@ import mongoose from "mongoose";
 import chalk from "chalk";
 
 mongoose.Promise = require('bluebird');
+//code xida ai sua dc thi sua
 let url = "mongodb://localhost:27017/mern";
-
-if (process.env.PORT) {
-    url = "mongodb://mahteam:mahteam@ds033976.mlab.com:33976/heroku_xb01qh9s";
-}
 if(process.env.NODE_ENV==="test"){
    url = "mongodb://localhost:27017/mern_test";
 }
+if (process.env.PORT) {
+    url = "mongodb://mahteam:mahteam@ds033976.mlab.com:33976/heroku_xb01qh9s";
+    if(process.env.NODE_ENV==="test"){
+       url = "REMOTE TEST DATABASE AI DK DIEN VAO";
+    }
+}
+
 
 mongoose.connect(url);
 
